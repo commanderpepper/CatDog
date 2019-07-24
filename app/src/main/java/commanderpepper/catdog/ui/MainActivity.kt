eb4dd14e0b22e72dc053ab2c9bcb6cecea4b87f3
commanderpepepr <ahmadhumza7@gmail.com>
2019-07-23 23:43:47 -0400

    Added basic outline for favorites

--- app/src/main/java/commanderpepper/catdog/ui/MainActivity.kt
+++ app/src/main/java/commanderpepper/catdog/ui/MainActivity.kt
@@ -1,28 +1,47 @@
 package commanderpepper.catdog.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActionBar
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageButton
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
-import com.bumptech.glide.Glide
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import commanderpepper.catdog.CatDogConstants
 import commanderpepper.catdog.Choice
 import commanderpepper.catdog.R
 import commanderpepper.catdog.databinding.ActivityMainBinding
-import commanderpepper.catdog.repo.CatDogRepository
-import commanderpepper.catdog.viewmodel.CatDogMainViewModel
-import kotlinx.android.synthetic.main.main_fragment.view.*
-import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Set Content View
-        DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
         Log.d(CatDogConstants.dogTag, Choice.DOG.toString())
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_items, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.cat_nav -> Toast.makeText(applicationContext, "Cat", Toast.LENGTH_SHORT).show()
+            R.id.dog_nav -> Toast.makeText(applicationContext, "Dog", Toast.LENGTH_SHORT).show()
+            R.id.both_nav -> Toast.makeText(applicationContext, "Both", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
